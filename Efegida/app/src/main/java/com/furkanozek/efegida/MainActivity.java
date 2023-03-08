b44f752dd283ae87b145cbbc3b0131de76620fa7
Furkan Özek <113854771+Furkan457@users.noreply.github.com>
2023-03-08 20:30:20 +0300

    Add files via upload

---
+++ Efegida/app/src/main/java/com/furkanozek/efegida/MainActivity.java
@@ -0,0 +1,14 @@
+package com.furkanozek.efegida;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
